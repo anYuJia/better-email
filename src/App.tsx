e95f03a3b61c197db503f0d9be701f742c53a3ea
anYuJia <92623305+anYuJia@users.noreply.github.com>
2026-08-15 10:27:22 +0800

    refactor: move pass notifications to component stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -104,6 +104,7 @@ const SnoozePicker = lazy(() => import('./components/SnoozePicker'));
 import DeferredSurface from './components/DeferredSurface';
 import SettingsOverlay from './components/settings/SettingsOverlay';
 const ShortcutHelpModal = lazy(() => import('./components/ShortcutHelpModal'));
+import './components/notifications.css';
 
 import {
   buildMailboxListStateKey,
